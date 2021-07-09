09527424397670c31d03083f8946329c1e950357
zack-mwangi <zackmwangi998@gmail.com>
2021-07-09 14:55:02 +0300

    first Commit

--- app/src/main/java/com/example/sqliteloginandreg/MainActivity.java
+++ app/src/main/java/com/example/sqliteloginandreg/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.sqliteloginandreg;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     @Override
